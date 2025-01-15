c790e3c390cdf3248504a37172f645672f237383
Christo Sheppard <c.sheppard@rijkzwaan.nl>
2025-01-15 20:41:18 +0100

    feat: Add filtering functionality to dataset list
    Implemented filtering feature to display datasets based on specific criteria (e.g., status).
    Added methods to handle filtering logic in the component. Updated table data source to reflect filtered datasets.

--- src/app/components/dataset-list.component.ts
+++ src/app/components/dataset-list.component.ts
@@ -1,9 +1,9 @@
 import { Component, inject, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { Observable, Subject, takeUntil } from 'rxjs';
 import { Dataset } from '../models/dataset.model';
-import { DatasetFacade } from '../models/facades/dataset.facade';
+import { DatasetFacade } from '../facades/dataset.facade';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
-import { DatePipe } from '@angular/common';
+import { DatePipe, NgClass } from '@angular/common';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import {MatCardModule} from '@angular/material/card';
@@ -12,13 +12,15 @@ import {MatCardModule} from '@angular/material/card';
   selector: 'app-dataset-list',
   templateUrl: './dataset-list.component.html',
   styleUrls:['./dataset-list.component.scss'],
-  imports: [DatePipe, MatTableModule, MatPaginator, MatCardModule],
+  imports: [DatePipe, MatTableModule, MatPaginator, MatCardModule, NgClass],
 })
 export class DatasetListComponent implements OnInit, OnDestroy {
   datasets$!: Observable<MatTableDataSource<Dataset>>;
   datasets: Dataset[] = [];
   displayedColumns: string[] = ['id', 'name', 'uploadedBy', 'updateDate', 'actions'];
   dataSource: MatTableDataSource<Dataset> = new MatTableDataSource();
+
+  selectedCard: string = '';
 
   @ViewChild(MatPaginator, { static: true }) paginator!: MatPaginator;
   @ViewChild(MatSort, { static: true }) sort!: MatSort;
@@ -49,5 +51,12 @@ export class DatasetListComponent implements OnInit, OnDestroy {
   ngOnDestroy() {
     this.destroy$.next();
     this.destroy$.complete();
+  }
+
+  // Method to filter datasets based on status
+  filterDatasetsByStatus(status: string) {
+    this.selectedCard = status;
+    const filteredDatasets = this.datasets.filter(dataset => dataset.status === status);
+    this.dataSource.data = filteredDatasets;
   }
 }
