3de21367dda40734d54f8e4a7e25e9ddd8758dc9
Christo Sheppard <c.sheppard@rijkzwaan.nl>
2025-01-16 22:20:12 +0100

    Implemented status count in status card

--- src/app/components/status-card/status-card.component.ts
+++ src/app/components/status-card/status-card.component.ts
@@ -1,25 +1,33 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit, inject } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
-import { NgClass } from '@angular/common';
+import { AsyncPipe, NgClass } from '@angular/common';
 import { StatusText } from '../../enums/status.enum';
-
-
+import { Observable } from 'rxjs';
+import { DatasetFacade } from '../../facades/dataset.facade';
 @Component({
   selector: 'status-card',
   templateUrl: './status-card.component.html',
   styleUrls: ['./status-card.component.scss'],
-  imports: [MatCardModule, NgClass],
+  imports: [MatCardModule, NgClass, AsyncPipe],
 })
-export class StatusCardComponent {
-  @Input() status!: string;
-  @Input() selectedStatus!: string;
-  @Output() statusSelected = new EventEmitter<string>();
+export class StatusCardComponent implements OnInit {
+  @Input() statusInput!: string;
+  @Input() selectedStatusInput!: string;
+  @Output() statusSelectedOutput = new EventEmitter<string>();
+
+  statusCount$!: Observable<number>;
+
+  private datasetFacade = inject(DatasetFacade);
+
+  ngOnInit(): void {
+    this.statusCount$ = this.datasetFacade.getStatusCount(this.statusInput);
+  }
 
   statusDisplayText(status: string): string {
     return StatusText[status as keyof typeof StatusText] || status;
   }
 
   onCardClick(status: string) {
-    this.statusSelected.emit(status);
+    this.statusSelectedOutput.emit(status);
   }
 }
