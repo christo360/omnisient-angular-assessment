4f91ea82a8879e664cb559b0dd496dc2b20d3425
Christo Sheppard <c.sheppard@rijkzwaan.nl>
2025-01-16 21:38:08 +0100

    refactor: show status text and implemented proper styling

--- src/app/components/status-card/status-card.component.ts
+++ src/app/components/status-card/status-card.component.ts
@@ -1,17 +1,23 @@
-import { NgClass } from '@angular/common';
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { NgClass } from '@angular/common';
+import { StatusText } from '../../enums/status.enum';
+
 
 @Component({
   selector: 'status-card',
   templateUrl: './status-card.component.html',
   styleUrls: ['./status-card.component.scss'],
-  imports:[MatCardModule, NgClass]
+  imports: [MatCardModule, NgClass],
 })
 export class StatusCardComponent {
   @Input() status!: string;
   @Input() selectedStatus!: string;
   @Output() statusSelected = new EventEmitter<string>();
+
+  statusDisplayText(status: string): string {
+    return StatusText[status as keyof typeof StatusText] || status;
+  }
 
   onCardClick(status: string) {
     this.statusSelected.emit(status);
---
+++ src/app/enums/status.enum.ts
@@ -0,0 +1,5 @@
+export enum StatusText {
+  review = 'Dataset Reviews',
+  fail = 'Failed Uploads',
+  shares = 'Shares by Company',
+}
