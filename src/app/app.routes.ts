a75bf0ba1861afd79c49c1663671205897d4a079
Christo Sheppard <c.sheppard@rijkzwaan.nl>
2025-01-16 20:05:17 +0100

    feat: added styling variables as well as added the poppins google font

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { DatasetListComponent } from './components/dataset-list.component';
+import { DatasetListComponent } from './components/dataset-list/dataset-list.component';
 
 export const routes: Routes = [
   { path: '**', component: DatasetListComponent },
